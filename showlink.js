1c015d262aaed64b6e0e09034dbcfb47a3670509
Lming1 <spear131121@gmail.com>
2017-06-03 16:46:41 +0900

    get link add

---
+++ showlink.js
@@ -0,0 +1,18 @@
+/**
+ * Created by iminhyeok on 2017. 6. 3..
+ */
+
+
+var client = require('cheerio-httpcli');
+
+var url = "http://jpub.tistory.com";
+var param = {};
+
+client.fetch(url, param, function (err, $, res) {
+    if(err){console.log("error"); return;}
+    $("a").each(function (idx) {
+        var text = $(this).text();
+        var href = $(this).attr('href');
+        console.log(text+":"+href);
+    });
+});
