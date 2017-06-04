4cee43eba345c1fc34a6ad02d1e6dd4614c9d384
Lming1 <spear131121@gmail.com>
2017-06-05 01:23:36 +0900

    mkdir and xml parsing add

---
+++ 6.5/test-xml.js
@@ -0,0 +1,13 @@
+/**
+ * Created by iminhyeok on 2017. 6. 5..
+ */
+
+var parseString = require('xml2js').parseString;
+
+var xml = "<fruits shop='AAA'>" +
+        "<item price='140'>Banana</item>" +
+        "<item price='200'>Apple</item>" +
+        "</fruits>";
+parseString(xml, function (err, result) {
+    console.log(JSON.stringify(result));
+});
