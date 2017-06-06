0d42a5d68e5f5dc9ea84b146dae44b52bfb08eae
Lming1 <spear131121@gmail.com>
2017-06-07 01:02:42 +0900

    6/6

---
+++ easy-app/main.js
@@ -0,0 +1,14 @@
+var TARGET_URL = "https://ko.wikipedia.org/";
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+app.on('ready', function(){
+	win = new BrowserWindow({
+		width: 800,
+		height: 600
+		});
+	win.loadURL(TARGET_URL);
+});
+
