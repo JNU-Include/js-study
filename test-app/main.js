6add32cce8360b988bd25bda57c3651b541508b6
Lming1 <spear131121@gmail.com>
2017-06-07 13:44:06 +0900

    index.html add

--- test-app/main.js
+++ test-app/main.js
@@ -2,4 +2,13 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 
+app.on('ready', function(){
+	win = new BrowserWindow({
+		width: 800, height: 600});
+	win.loadURL('file://' + __dirname + 'index.html');
+	win.on('closed', function(){
+		win = null;
+		});
+});
+
 
